f367ad026e576421b93d3de09c7da68bd9dbef72
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:25:32 +0200

    created example of code

--- StopCommandMessage/code/code_for_using_dataModel.AutonomousMobileRobot_StopCommandMessage.py
+++ StopCommandMessage/code/code_for_using_dataModel.AutonomousMobileRobot_StopCommandMessage.py
@@ -24,19 +24,24 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "StopCommandMessage"
 subject = "dataModel.AutonomousMobileRobot"
-commandTime = "{'type': 'Property', 'value': {'@type': 'Date-Time', '@value': '2019-06-07T08:39:40.064+09:00'}}"
+commandTime = "2019-06-07T08:39:40.064+09:00"
 attribute = "commandTime"
 value = commandTime
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-stopCommand = "{'type': 'Property', 'value': 'stop'}"
+stopCommand = "stop"
 attribute = "stopCommand"
 value = stopCommand
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
